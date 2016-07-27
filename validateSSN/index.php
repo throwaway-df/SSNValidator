5a103219b3f7825e6b991a8a67fe0a6eda1f5243
throwaway-df <throwawaydf123@gmail.com>
2016-07-28 01:02:13 +0300

    added SSN parsing

--- validateSSN/index.php
+++ validateSSN/index.php
@@ -1,7 +1,35 @@
 <?php
 
+  // SSNValidator class
   require_once('ssnvalidator.php');
 
-  $ssnValidator = new SSNValidator();
+  $errorCode;
+  $errorText;
+  $value = true;
+
+  // Handle URL parameters
+  if(isset($_GET['ssn'])) {
+
+    $ssn = $_GET['ssn'];
+
+    $ssnValidator = new SSNValidator($ssn);
+
+  } else {
+
+    // Set value to false, set error and parse response
+
+  }
+
+  // Start SSN Validation
+
+  if(!$ssnValidator->parseSSN()) {
+
+    // Set value to false and parse response
+
+  }
+
+
+
+
 
 ?>
--- validateSSN/ssnvalidator.php
+++ validateSSN/ssnvalidator.php
@@ -6,6 +6,22 @@ class SSNValidator {
     $this->ssn = $ssn;
   }
 
+  public function parseSSN() {
+
+    $uppercaseSSN = strtoupper($this->ssn);
+
+    $pattern = "/^\d{6}[-A]\d{3}[A-Z0-9]$/";
+
+    if(!preg_match($pattern, $uppercaseSSN)) {
+
+      return false;
+
+    }
+
+    return true;
+
+  }
+
 }
 
 ?>
